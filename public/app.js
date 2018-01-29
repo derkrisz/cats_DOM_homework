1666519981afff662acb1db8ec3f5bfa8ae47012
derkrisz <krisztian.dergmail.com>
2018-01-29 22:15:41 +0000

    put cat data into an array

--- public/app.js
+++ public/app.js
@@ -42,12 +42,29 @@ var addCat = function(name, fav_food, image) {
   appendElements(cats, newUnorderedList, nameListItem, favFoodListItem, imageListItem);
 }
 
+var catsArray = [
+  {name: "Boba", favourite_food: "Sock fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", favourite_food: "Tuna", image: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: "Max", favourite_food: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  {name: "Cirom", favourite_food: "corn on the cob", image: "/cirmos.jpg"},
+  {name: "Domber", favourite_food: "boiled potatoes", image: "/domber.jpg"}
+]
+
 var app = function() {
-  addCat("Cirom", "corn on the cob", "/cirmos.jpg");
-  addCat("Domber", "boiled potatoes", "/domber.jpg")
+  for (cat of catsArray) {
+    addCat(cat.name, cat.favourite_food, cat.image);
+  }
 }
 
+// add a cat just by calling addcat (no array);
 
+// var app = function() {
+//   addCat("Cirom", "corn on the cob", "/cirmos.jpg");
+//   addCat("Domber", "boiled potatoes", "/domber.jpg")
+// }
+
+
+// harcoded function to add a cat
 
 // var app = function() {
 //   var catUnorderedList = document.createElement('ul');
@@ -55,7 +72,6 @@ var app = function() {
 //
 //   var listItem = document.createElement('li');
 //   listItem.innerText = "Name: Cirom";
-//
 //
 //   var listItem2 = document.createElement('li');
 //   listItem2.innerText = "Favourite food: corn on the cob";
