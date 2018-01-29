517f94566318cec1ba73ca4dfdc715a1700c29e9
derkrisz <krisztian.dergmail.com>
2018-01-29 21:37:31 +0000

    refactor add new cat (flexible)

--- public/app.js
+++ public/app.js
@@ -1,27 +1,80 @@
-var app = function() {
+
+var createCatUnorderedList = function() {
   var catUnorderedList = document.createElement('ul');
   catUnorderedList.classList.add('cat');
+  return catUnorderedList;
+}
 
-  var listItem = document.createElement('li');
-  listItem.innerText = "Name: Cirom";
+var createNameListItem = function(name) {
+  var nameListItem = document.createElement('li');
+  nameListItem.innerText = "Name: " + name;
+  return nameListItem;
+}
 
-  var listItem2 = document.createElement('li');
-  listItem2.innerText = "Favourite food: corn on the cob";
+var createFavFoodListItem = function(fav_food) {
+  var favFoodListItem = document.createElement('li');
+  favFoodListItem.innerText = "Favourite food: " + fav_food;
+  return favFoodListItem;
+}
 
-  var listItem3 = document.createElement('li');
-
+var createImageListItem = function(image_source) {
+  var imageListItem = document.createElement('li');
   var img = document.createElement('img');
-  img.src = "/cirmos.jpg"
+  img.src = image_source;
   img.width = "500";
+  imageListItem.appendChild(img);
+  return imageListItem;
+}
 
-  listItem3.appendChild(img);
-
-  catUnorderedList.appendChild(listItem);
-  catUnorderedList.appendChild(listItem2);
-  catUnorderedList.appendChild(listItem3);
-
-  var cats = document.querySelector('#cats');
+var appendElements = function(cats, catUnorderedList, nameListItem, favFoodListItem, imageListItem) {
+  catUnorderedList.appendChild(nameListItem);
+  catUnorderedList.appendChild(favFoodListItem);
+  catUnorderedList.appendChild(imageListItem);
   cats.appendChild(catUnorderedList);
 }
+
+var addCat = function(name, fav_food, image) {
+  var newUnorderedList = createCatUnorderedList();
+  var nameListItem = createNameListItem(name);
+  var favFoodListItem = createFavFoodListItem(fav_food);
+  var imageListItem = createImageListItem(image);
+  var cats = document.querySelector('#cats');
+  appendElements(cats, newUnorderedList, nameListItem, favFoodListItem, imageListItem);
+}
+
+var app = function() {
+  addCat("Cirom", "corn on the cob", "/cirmos.jpg");
+  addCat("Domber", "boiled potatoes", "/domber.jpg")
+}
+
+
+
+// var app = function() {
+//   var catUnorderedList = document.createElement('ul');
+//   catUnorderedList.classList.add('cat');
+//
+//   var listItem = document.createElement('li');
+//   listItem.innerText = "Name: Cirom";
+//
+//
+//   var listItem2 = document.createElement('li');
+//   listItem2.innerText = "Favourite food: corn on the cob";
+//
+//   var listItem3 = document.createElement('li');
+//
+//   var img = document.createElement('img');
+//   img.src = "/cirmos.jpg"
+//   img.width = "500";
+//
+//   listItem3.appendChild(img);
+//
+//   catUnorderedList.appendChild(listItem);
+//   catUnorderedList.appendChild(listItem2);
+//   catUnorderedList.appendChild(listItem3);
+//
+//   var cats = document.querySelector('#cats');
+//   cats.appendChild(catUnorderedList);
+// }
+
 
 document.addEventListener("DOMContentLoaded", app);
